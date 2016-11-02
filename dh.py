0b1368b40cc6bcabe504bc4e079233c700915e16
Vishnu Prem <vishnu@vishnuprem.com>
2016-11-02 17:51:26 +0800

    Generate prime

--- dh.py
+++ dh.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python
+import random
 
-print "Hi Travis!"
+class DH(object):
+    def __init__(self):
+        prime = self.largePrime()
+        print prime
+        
+    def largePrime(self):
+      primeFound = False
+      while primeFound == False:
+        n = random.randint(100000000, 1000000000)
+        if n % 2 != 0:
+          for x in range(3, int(n**0.5), 2):
+            if n % x ==0:
+              break
+          else:
+              primeFound = True
+      return n
+        
+a = DH()
